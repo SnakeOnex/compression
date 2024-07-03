e18dae01c2e77f061d511185cbd9e9446c87b230
snakeonex <romakcz@gmail.com>
2024-07-03 17:07:19 +0200

    dfs -> really bad encoding

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def huffman_coding(counts):
     # tree node representation -> (val, left, right)
     sort_forest = lambda f : sorted(f, key=lambda x: x[0][1])
     forest = sort_forest([((key, val/ln), None, None) for key, val in counts.items()])
-    print(forest[:10])
+    # print(forest[:10])
 
     print(len(forest))
 
@@ -18,10 +18,13 @@ def huffman_coding(counts):
 
         forest = [new_tree] + forest[2:]
         forest = sort_forest(forest)
-        print(forest[:10])
-        print(len(forest))
+        # print(forest[:10])
+        # print(len(forest))
         iter_count += 1
-        if iter_count == 200: break
+        # if iter_count == 200: break
+
+    return forest[0]
+
 
 filename = "enwik6"
 
@@ -51,6 +54,41 @@ for i, (key, value) in enumerate(counts.items()):
 entropy = -sum([log2(c/len(data))*(c/len(data)) for _, c in counts.items()])
 print(f"entropy = {entropy:.3f} bits")
 print(f"worst case entropy for {len(symbols)} symbols: {log2(len(symbols)):.3f} bits")
-print(f"optimal compression ratio: 8 / {entropy:.3f} = {8 / entropy:.2f}x")
+print(f"optimal compression ratio (for byte level compression): 8 / {entropy:.3f} = {8 / entropy:.2f}x")
 
-huffman_coding(counts)
+tree = huffman_coding(counts)
+
+# print(tree)
+
+encoded = []
+
+def dfs(tree, path):
+    if tree[1] is None and tree[2] is None:
+        # print(f"Symbol: {tree[0][0]} Path: {path}")
+        encoded.append((tree[0][0],path))
+    else:
+        dfs(tree[1], path + "0")
+        dfs(tree[2], path + "1")
+
+dfs(tree, "")
+
+encoded = sorted(encoded, key=lambda x: len(x[1]))
+encoder = {s: code for s, code in encoded}
+decoder = {code: s for s, code in encoded}
+
+for i in range(20):
+    print(f"{i+1:2}: s={encoded[i][0]:3} chr={repr(chr(encoded[i][0])):3} code={encoded[i][1]}")
+
+encoded_data = ""
+
+for s in data:
+    encoded_data += encoder[s]
+
+# print(data[:100])
+# print data in binary
+# print(" ".join(f"{b:08b}" for b in data[:10]))
+# print(encoded_data)
+
+
+
+
